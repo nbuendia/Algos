f0bea03b8fca0c9067976fbef041de9321f26fa2
nbuendia <nicole.buendia424@gmail.com>
2021-12-08 21:01:05 -0500

    All tests passed - 2 responses

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+// function reverseInt(n) {
+//     const reversedNum = Math.abs(n).toString().split('').reverse();
+//     if (n <= 9 && n >= 0) return n;
+//     if (n < 0) return Number(reversedNum.join('')) * -1;
+//     return Number(reversedNum.join(''));
+// }
+
+function reverseInt(n) {
+    const reversedNum = Math.abs(n).toString().split('').reverse();
+    return (n <= 9 && n >= 0) ? n 
+         : (n < 0) ? Number(reversedNum.join('')) * -1 
+         : Number(reversedNum.join(''));
+}
 
 module.exports = reverseInt;
