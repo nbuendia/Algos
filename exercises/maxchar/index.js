40e840794631b81c401014524f27639fae3472cf
nbuendia <nicole.buendia424@gmail.com>
2021-12-09 10:34:11 -0500

    All tests passed - 1 response

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,23 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    if (str.length === 1) return str; 
+    const charObj = {};
+
+    for(let i = 0; i < str.length; i++) {
+        let result = Object.keys(charObj).find(char => char === str[i]);
+        if (!!result) {
+            charObj[str[i]]++;
+        } else {
+            charObj[str[i]] = 1;
+        }
+    }
+
+    const idx = Object.values(charObj).findIndex(value => 
+        value === Math.max(...Object.values(charObj)));
+
+    return Object.keys(charObj)[idx];
+}
 
 module.exports = maxChar;
