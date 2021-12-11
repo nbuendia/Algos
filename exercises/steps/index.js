68c334e8ba02fa3e8b51500723efc1593b6741b2
nbuendia <nicole.buendia424@gmail.com>
2021-12-10 19:25:41 -0500

    All tests passed - 2 resposes

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,33 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+//FUNCTION 1
+function steps(n) {
+    let poundArr = [];
+    let spaceCount = 1;
+
+    for (let i = 0; i < n; i++) poundArr[i] = Array(n).fill('#');
+
+    poundArr.map((arr) => {
+        console.log(arr.fill(' ', spaceCount).join(''));
+        spaceCount++;
+    });
+}
+
+//FUNCTION 2
+// function steps(n) {
+//     let poundArr = [];
+//     let spaceArr = [];
+
+//     for (let i = 0; i < n; i++) poundArr[i] = Array(i + 1).fill('#');
+//     for (let i = 0; i <= n - 2; i++) spaceArr[i] = Array(i + 1).fill(' ');
+//     spaceArr.reverse();
+
+//     for (let i = 0; i <= n - 1; i++) {
+//         if (i === n - 1) console.log(poundArr[i].join(''));
+//         else console.log(poundArr[i].concat(spaceArr[i]).join(''));
+//     }
+
+// }
 
 module.exports = steps;
