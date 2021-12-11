3351b7d563f3e6c4cedf3a99a38cb4f7949d722e
nbuendia <nicole.buendia424@gmail.com>
2021-12-10 21:48:40 -0500

    All tests passed - 1 response

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,17 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+    const vowels = 'aeiou';
+    let count = 0;
+    let i = 0;
+
+    while (i < str.length) {
+        if (vowels.includes(str[i].toLowerCase())) count++;
+        i++;
+    }
+
+    return count;
+}
 
 module.exports = vowels;
